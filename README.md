0e818cad2f9bd13f437ff3f22ade5adbda215368
Kama2111 <153067902+Kamran2111@users.noreply.github.com>
2024-12-27 13:47:51 +0400

    new create project

---
+++ README.md
@@ -0,0 +1,58 @@
+# Movie movieHub
+
+This project is a simple movie list application built with **Vite**, **HTML**, **CSS**, and **JavaScript**. It includes features such as:
+
+- Displaying a list of movies.
+- Animations for dynamic effects.
+- CSS for responsive and modern styling.
+
+## Features
+
+- **Movie List:** View movies with their titles and images.
+- **Animation:** Smooth animations for better user experience.
+- **Responsive Design:** Built with CSS to ensure compatibility with various devices.
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Kamran2111/MovieHub.git
+   ```
+
+2. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+## Running the Project
+
+Start the development server:
+
+```bash
+npm run dev
+```
+
+Open the application in your browser at `http://localhost:5173`.
+
+## Project Structure
+
+```
+movieHub/
+├── public/
+├── src/
+│   ├─  main.js
+│   ├── style.css
+├── index.html
+├── package.json           # Dependencies and scripts
+├── vite.config.ts         # Vite configuration
+└── README.md              # Project documentation
+```
+
+## Scripts
+
+- `npm install`: Install dependencies.
+- `npm run dev`: Run the development server.
+- `npm run build`: Build the project for production.
+- `npm run preview`: Preview the production build.
---
+++ src/main.js
@@ -0,0 +1,157 @@
+const API_KEY = "c38d3589-6731-45cb-9483-6c88d38dec88";
+const API_URL_POPULAR =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_MOVIES&page=";
+const API_URL_SEARCH =
+  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+
+const API_URL_MOVIE_DETAILS =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/";
+
+let currentPage = 1;
+
+getMovies(API_URL_POPULAR + currentPage);
+
+async function getMovies(url) {
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
+  const responseData = await response.json();
+  showMovies(responseData);
+}
+
+function getClassByRate(vote) {
+  return vote >= 7 ? "green" : vote > 5 ? "orange" : "red";
+}
+
+function showMovies(data) {
+  const moviesElement = document.querySelector(".movies");
+  moviesElement.innerHTML = "";
+
+  if (data?.films || data?.items) {
+    const movies = data.films || data.items;
+    movies.forEach((movie) => {
+      const movieElement = document.createElement("li");
+      movieElement.classList.add("movie");
+      movieElement.setAttribute("data-id", movie.kinopoiskId);
+      const ratingClass = movie.ratingKinopoisk
+        ? getClassByRate(movie.ratingKinopoisk)
+        : getClassByRate();
+
+      movieElement.innerHTML = `
+        <div class="movie__cover-inner">
+          <img
+            src="${movie.posterUrl}"
+            alt="${movie.nameRu}"
+            class="movie__poster"
+          />
+          <div class="movie__cover-dark"></div>
+        </div>
+        <div class="movie__info">
+          <h3 class="movie__title">${movie.nameRu}</h3>
+          <p class="movie__genre">${movie.genres
+            .map((genre) => ` ${genre.genre}`)
+            .join(", ")}</p>
+          <p class="movie__rating ${ratingClass}">
+            ${movie.ratingKinopoisk || ""}
+          </p>
+        </div>
+      `;
+
+      movieElement.addEventListener("click", () =>
+        openModal(movie.kinopoiskId)
+      );
+
+      moviesElement.append(movieElement);
+    });
+  } else {
+    moviesElement.innerHTML = "<p>No movies found.</p>";
+  }
+}
+
+const search = document.querySelector(".header__search");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchValue = search.value;
+  const apiSearchUrl = `${API_URL_SEARCH}${searchValue}`;
+  if (searchValue) {
+    getMovies(apiSearchUrl);
+  }
+});
+const prevPageBtn = document.getElementById("prevPage");
+const nextPageBtn = document.getElementById("nextPage");
+
+prevPageBtn.addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    getMovies(API_URL_POPULAR + currentPage);
+  }
+});
+
+nextPageBtn.addEventListener("click", () => {
+  currentPage++;
+  getMovies(API_URL_POPULAR + currentPage);
+});
+
+async function openModal(id) {
+  const resp = await fetch(API_URL_MOVIE_DETAILS + id, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
+
+  const respData = await resp.json();
+  const modalEl = document.querySelector(".modal");
+
+  modalEl.classList.add("modal__show");
+  document.body.classList.add("stop-scrolling");
+
+  modalEl.innerHTML = `
+<div class="modal__card">
+  <img src="${respData.posterUrl}" alt="" class="modal__movie-backdrop" />
+  <h2>
+    <span class="modal__movie--title">${respData.nameRu}</span>
+    <span class="modal__movie--release-year">- ${respData.year}</span>
+  </h2>
+  <ul class="modal__movie--info">
+    <li class="modal__movie--genre">Жанр - ${respData.genres
+      .map((el) => `<span>${el.genre} </span>`)
+      .join("")}</li>
+    <li class="modal__movie--runtime">Время - ${respData.filmLength} минут</li>
+    <li class="modal__link">Сайт :  <a href="${
+      respData.webUrl
+    }" class="modal__movie--site">Смотреть</a></li>
+    <li class="modal__movie-overview">Описание - ${respData.description}</li>
+  </ul>
+  <button class="modal__button-close">Закрыть</button>
+</div>
+`;
+
+  const btnClose = modalEl.querySelector(".modal__button-close");
+  btnClose.addEventListener("click", () => closeModal(modalEl));
+}
+
+function closeModal(modalEl) {
+  modalEl.classList.remove("modal__show");
+  document.body.classList.remove("stop-scrolling");
+}
+
+window.addEventListener("click", (e) => {
+  const modalEl = document.querySelector(".modal");
+  if (e.target === modalEl) {
+    closeModal(modalEl);
+  }
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    const modalEl = document.querySelector(".modal");
+    closeModal(modalEl);
+  }
+});
